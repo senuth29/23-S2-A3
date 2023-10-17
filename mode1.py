577840f2da7f2cbd916e13b2c753054bc39e8674
senuth29 <dilnaka.dias@gmail.com>
2023-10-17 20:36:01 +1100

    Started on task 1

--- mode1.py
+++ mode1.py
@@ -1,4 +1,5 @@
 from island import Island
+from data_structures.bst import BinarySearchTree
 
 class Mode1Navigator:
     """
@@ -8,23 +9,65 @@ class Mode1Navigator:
     def __init__(self, islands: list[Island], crew: int) -> None:
         """
         Student-TODO: Best/Worst Case
+        Best case -> 
+        Worst case -> O(nlog(n)) or less
         """
-        raise NotImplementedError()
+        self.island_bst = BinarySearchTree()
+        for island in islands:
+            self.island_bst[island.money / island.marines] = island
+        
+        self.crew = crew
+
 
     def select_islands(self) -> list[tuple[Island, int]]:
         """
         Student-TODO: Best/Worst Case
+        Best case -> O(log(n)) or less
+        Worst case -> O(n) or less
         """
-        raise NotImplementedError()
+        selected_islands = []
+        remaining_crew = self.crew
+
+        while remaining_crew > 0 and not self.island_bst.is_empty():
+            island = self.island_bst.root.item
+            if island > 0:
+                pirates_sent = min(remaining_crew, island.marines)
+                selected_islands.append(island, pirates_sent)
+                remaining_crew -= pirates_sent
+                del self.island_bst[island]
+                
+        return selected_islands
 
     def select_islands_from_crew_numbers(self, crew_numbers: list[int]) -> list[float]:
         """
         Student-TODO: Best/Worst Case
+        Best case ->
+        Worst case -> O(C * N) where C is the length crew numbers
         """
-        raise NotImplementedError()
+        res = []
+        for count in crew_numbers:
+            remaining_crew = count
+            total_money = 0
+
+            while remaining_crew > 0 and not self.island_bst.is_empty():
+                island = self.island_bst.root.item
+                if island.money > 0:
+                    pirates_sent = min(remaining_crew, island.marines)
+                    total_money += min(((pirates_sent * island.money)/island.marines), island.money)
+                    remaining_crew -= pirates_sent
+            res.append(total_money)
+
+        return res
+        
 
     def update_island(self, island: Island, new_money: float, new_marines: int) -> None:
         """
         Student-TODO: Best/Worst Case
+        Best case ->
+        Worst case -> O(log(n)) or less
         """
-        raise NotImplementedError()
+        if new_money > 0:
+            del self.island_bst[island.money / island.marines]
+            island.money = new_money
+            island.marines = new_marines
+            self.island_bst[new_money / new_marines] = island
